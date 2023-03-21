13d5e7af852af10f724cae4fd64159c3011acf7a
Evgeniy-Muhamethin <John090918@gmail.com>
2023-03-21 23:39:52 +0300

    Leason was done. Home work is need to do

--- StartUnity/Assets/Scripts/ExampleScripts.cs
+++ StartUnity/Assets/Scripts/ExampleScripts.cs
@@ -4,32 +4,24 @@ using UnityEngine;
 
 public class ExampleScripts : MonoBehaviour
 {
-    /// <summary>
-    /// 
-	///TODO Скриптинг, работа с объектами компонентами
-    /// </summary>
+    InstantiateOrDestroyObject _InstantiateOrDestroyObject;
 
-		InstantiateOrDestroyObject _InstantiateOrDestroyObject;
-
-    [SerializeField] private GameObject _gameObject;
-
-    private int _timeDestroy;
-
-    void Awake()
-    {
-        _InstantiateOrDestroyObject = new InstantiateOrDestroyObject();
-    }
+    [SerializeField] private Vector3 _gameObject;
+    [SerializeField] private float _speedPlayer;
 
     void Start()
     {
-        _InstantiateOrDestroyObject.InstantiateObject(_gameObject, 
-            Vector3.one, new Quaternion(1,2,1,1));
-
-        _InstantiateOrDestroyObject.DestroyObject(_gameObject, _timeDestroy);
-    }
+		_InstantiateOrDestroyObject = new
+			InstantiateOrDestroyObject();
+	}
 
     void Update()
     {
-        
-    }
+		//Движение персонажа на основе Axis
+		_gameObject.x = Input.GetAxis("Horizontal");
+		_gameObject.y = Input.GetAxis("Vertical");
+
+		var speed = _gameObject * _speedPlayer * Time.deltaTime;
+        transform.Translate(speed);
+	}
 }
--- StartUnity/Assets/Scripts/Test/InstantiateOrDestroyObject.cs
+++ StartUnity/Assets/Scripts/Test/InstantiateOrDestroyObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InstantiateOrDestroyObject
@@ -14,4 +15,34 @@ public class InstantiateOrDestroyObject
     {
         MonoBehaviour.Destroy(gameObject, timeDestroy);
     }
+
+    public void SetActiveObject(GameObject gameObject, bool flag)
+    {
+        gameObject.SetActive(flag);
+    }
+
+    //отключение родительского объекта совсей его ирархией
+	public void SetActiveObject(Transform transform,bool flag)
+	{
+		transform.parent.gameObject.SetActive(flag);
+	}
+
+	public void SetActiveObject(Transform transform, bool flag, int numberChild)
+	{
+		transform.GetChild(numberChild).gameObject.SetActive(flag);
+	}
+
+    public bool FindGameObjectTags(string tags)
+    {
+        bool result = false;
+        GameObject.FindGameObjectWithTag(tags);
+        Debug.Log($"{tags.ToString()} is find");
+        return result = true;
+    }
+
+	public int LayersTesting(GameObject gameObject)
+	{
+        int result = gameObject.layer;
+        return result;
+	}
 }
