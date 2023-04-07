52ca322c9a28369012d054c6bba9ee6afdb7a8fb
Evgeniy-Muhamethin <John090918@gmail.com>
2023-04-07 20:21:42 +0300

    Continue

---
+++ StartUnity/Assets/Scripts/Lesson5/MainLesson.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainLesson : MonoBehaviour
+{
+	[SerializeField]
+	private Transform _transform;
+
+	private void FixedUpdate()
+	{
+		
+	}
+}
