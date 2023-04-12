0ac7010af51ce27b65193ba07409aee3ed26c569
Evgeniy-Muhamethin <John090918@gmail.com>
2023-04-12 08:14:48 +0300

    Invoke was learned

--- StartUnity/Assets/Scripts/Lesson5/MainLesson.cs
+++ StartUnity/Assets/Scripts/Lesson5/MainLesson.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class MainLesson : MonoBehaviour
@@ -7,13 +8,37 @@ public class MainLesson : MonoBehaviour
 	[SerializeField]
 	private Transform _transform;
 
+
+	private float _from = 1;
+	private float _to = 10;
+	
+	private float _time = 5.0f;
+
+	private delegate void RandomRageMethodDelegate();
+
+	private RandomRageMethodDelegate RandomRageDelegate;
+
 	private void Start()
 	{
-		
+		RandomRageDelegate = RandomRageMethod;
+		Invoke($"RandomRageMethod", _time);
 	}
+
+	float _repeatRate = 10f;
 
 	private void FixedUpdate()
 	{
-		
+		InvokeRepeating("RandomRageMethod", _time, _repeatRate);
+
+		if (Input.GetKey(KeyCode.Tab))
+		{
+			CancelInvoke();
+		}
+	}
+
+	private void RandomRageMethod()
+	{
+		float result = Random.Range(0f, 10f);
+		print(result);
 	}
 }
